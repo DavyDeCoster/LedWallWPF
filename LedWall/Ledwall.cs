1057c12a79a27596bbba5368d76e922112292ca4
KillermenBE <davy.de.coster@gmail.com>
2015-03-09 13:58:40 +0100

    Changed SByte, almost perfect

--- LedWall/Ledwall.cs
+++ LedWall/Ledwall.cs
@@ -13,21 +13,37 @@ namespace LedWall
 {
     class Ledwall
     {
+        static SerialPort port;
+        static sbyte[] data = new sbyte[107 * 48 * 3 + 3];
+        static bool _continue;
+
         public static void ReadPixels(string path)
         {
             List<System.Drawing.Color> lstColor = new List<Color>();
 
+            String[] Coms = SerialPort.GetPortNames();
+            port = new SerialPort();
+            port.PortName = Coms[0];
+
             Bitmap bmOriginal = new Bitmap("C:\\Users\\Davy\\Dropbox\\0.School\\Stage\\LedWallC#\\LedWall\\LedWall\\Images\\red.jpg");
             Bitmap bm = new Bitmap(bmOriginal, 107, 48);
-            byte[] data = new byte[107 * 48 * 3 + 3];
+
+            float gamma = 1.7F;
+            int[] gammatable = new int[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                gammatable[i] = (int)(Math.Pow((float)i / 255.0, gamma) * 255.0 + 0.5);
+            }
+
 
             Color[] pixel = new Color[8];
             int[] editedPixels = new int[8];
             int x, y, xbegin, xend, xinc, mask, offset = 3, linesPerPin = 3;
-            
-            for(y=0;y<linesPerPin;y++)
+
+            for (y = 0; y < linesPerPin; y++)
             {
-                if((y&1)==(0))
+                if ((y & 1) == (0))
                 {
                     xbegin = 0;
                     xend = 107;
@@ -39,47 +55,55 @@ namespace LedWall
                     xend = -1;
                     xinc = -1;
                 }
-                for (x = xbegin; x !=xend; x+=xinc)
+                for (x = xbegin; x != xend; x += xinc)
                 {
                     for (int i = 0; i < 8; i++)
                     {
                         pixel[i] = bm.GetPixel(x, y * linesPerPin);
-                        editedPixels[i] = colorWiring(pixel[i]);
+                        editedPixels[i] = colorWiring(pixel[i], gammatable);
                     }
 
                     for (mask = 0x800000; mask != 0; mask >>= 1)
                     {
-                        byte b = 0;
+                        sbyte b = 0;
                         for (int i = 0; i < 8; i++)
                         {
                             if ((editedPixels[i] & mask) != 0)
                             {
-                                b |= (byte)(1 << i);
+                                b |= (sbyte)(1 << i);
                             }
                         }
-
                         data[offset++] = b;
                     }
                 }
-                char ster = '*';
-                data[0] = System.Text.Encoding.ASCII.GetBytes(ster);
+                data[0] = (sbyte)System.Text.Encoding.ASCII.GetBytes("*")[0];
                 int usec = (int)((1000000.0 / 25) * 0.75);
-                data[1] = (byte)(usec);   // request the frame sync pulse
-                data[2] = (byte)(usec >> 8); // at 75% of the frame time
+                data[1] = (sbyte)(usec);   // request the frame sync pulse
+                data[2] = (sbyte)(usec >> 8); // at 75% of the frame time
             }
-            foreach (byte b in data)
+            port.Open();
+
+            _continue = true;
+
+            int j = 0;
+            foreach (sbyte b in data)
             {
-                Console.WriteLine(b);
+                Console.WriteLine(j + ": " + b);
+                j++;
             }
         }
 
-        private static int colorWiring(Color color)
+        private static int colorWiring(Color color, int[] gammatable)
         {
             int red = color.R;
             int green = color.G;
             int blue = color.B;
 
-            return (green << 16) | (red << 8) | (blue);
+            red = gammatable[red];
+            green = gammatable[green];
+            blue = gammatable[blue];
+
+            return ((green << 16) | (red << 8) | (blue));
         }
     }
 }

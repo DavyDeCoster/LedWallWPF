f7b769e5720da3a1980e675eed3f03e57dd85265
KillermenBE <davy.de.coster@gmail.com>
2015-06-03 18:19:32 +0200

    Fixed Ghosting Font

--- LedWall/TxtToImage.cs
+++ LedWall/TxtToImage.cs
@@ -18,11 +18,14 @@ namespace LedWall
                 Font font = new Font(fontname, fontsize);
                 graphics.FillRectangle(new SolidBrush(bgcolor), 0, 0, bmp.Width, bmp.Height);
                 graphics.DrawString(txt, font, new SolidBrush(fcolor), 0, 0);
+                graphics.DrawString(txt, font, new SolidBrush(fcolor), -1, 0);
+                graphics.DrawString(txt, font, new SolidBrush(fcolor), 1, 0);
                 graphics.Flush();
                 font.Dispose();
                 graphics.Dispose();
             }
-            return bmp;
+
+            return MakePixelsWhite(bmp);
         }
 
         static public Bitmap ConvertTextToImage(string txt, string fontname, int fontsize, Color bgcolor, Color fcolor, int width, int Height, int x, int y)
@@ -37,6 +40,27 @@ namespace LedWall
                 graphics.Flush();
                 font.Dispose();
                 graphics.Dispose();
+            }
+
+            return MakePixelsWhite(bmp);
+        }
+
+        private static Bitmap MakePixelsWhite(Bitmap bmp)
+        {
+            int lenX = bmp.Width;
+            int lenY = bmp.Height;
+            Color white = Color.White;
+
+            for (int xImage = 0; xImage < lenX; xImage++)
+            {
+                for (int yImage = 0; yImage < lenY; yImage++)
+                {
+                    Color pixel = bmp.GetPixel(xImage, yImage);
+                    if (pixel.R > 1 || pixel.G > 1 || pixel.B > 1)
+                    {
+                        bmp.SetPixel(xImage, yImage, white);
+                    }
+                }
             }
             return bmp;
         }
